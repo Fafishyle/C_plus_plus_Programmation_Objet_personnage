8585e5c864bd78d8295401a1a90c3e2d26d11301
Fafishyle <finaritralft@gmail.com>
2023-11-09 15:18:36 +0100

    Depot classe Personnage

---
+++ Personnage.cpp
@@ -0,0 +1,7 @@
+#include "Personnage.h"
+#include <iostream>
+
+Personnage::Personnage(std::string nomPerso, int viePerso) : nom(nomPerso), vie(viePerso) {
+	//vie = viePerso;
+	//nom = nomPerso;
+};
---
+++ Personnage.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class Personnage
+{
+	std::string nom;
+	int vie;
+
+public:
+	Personnage(std::string nom, int vie);
+};
+
