45b0f97f3a1315e8361f86f1e0aedc5043144fa4
Fafishyle <finaritralft@gmail.com>
2023-11-16 12:05:54 +0100

    TD4_Banque finit
    Affichage de la console en couleur

--- TD4_Banque/CompteBancaire.cpp
+++ TD4_Banque/CompteBancaire.cpp
@@ -1,6 +1,45 @@
 #include "CompteBancaire.h"
 #include <iostream>
 
+
+//______________________________POUR L AFFICHAGE DU TERMINAL EN COULEUR__________________________________________________________
+
+#include <string>
+#include <windows.h>
+
+class setcolor
+{
+private:
+    WORD m_attr;
+public:
+    explicit setcolor(WORD attr) : m_attr(attr) {}
+
+    template<typename _CharT, typename _Traits>
+    std::basic_ostream<_CharT, _Traits>& Apply(std::basic_ostream<_CharT, _Traits>& stm)
+    {
+        stm.flush();
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), m_attr);
+        return stm;
+    }
+};
+
+template<typename _CharT, typename _Traits>
+std::basic_ostream<_CharT, _Traits>& operator<<(std::basic_ostream<_CharT, _Traits>& stm, setcolor sc)
+{
+    return sc.Apply(stm);
+}
+
+
+WORD GetTextAttribute()
+{
+    auto info = CONSOLE_SCREEN_BUFFER_INFO();
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+    return info.wAttributes;
+}
+
+//________________________________POUR L AFFICHAGE DU TERMINAL EN COULEUR________________________________________________________
+
+
 CompteBancaire::CompteBancaire(int identifiant, double argent) : id(identifiant), solde(argent) {};
 
 void CompteBancaire::deposerArgent(double argent) {
@@ -14,5 +53,14 @@ bool CompteBancaire::retraitArgent(double argent) {
 
 void CompteBancaire::consulteSolde()
 {
-	std::cout << "Solde actuel	:	"<< solde << std::endl;
+    //Couleur blanche console par defaut
+    int Couleur_affichage = 0x07;
+
+    //Si le compte est negatif
+	if (!retraitArgent(0))
+	{
+        //Affiche en rouge
+        Couleur_affichage = 0x04; 
+	}
+	std::cout << "Solde actuel	:	"<< setcolor(Couleur_affichage)<< solde <<setcolor(0x07)<< std::endl;
 };
--- TD4_Banque/TD4_Banque.cpp
+++ TD4_Banque/TD4_Banque.cpp
@@ -2,8 +2,31 @@
 //
 
 #include <iostream>
+#include "CompteBancaire.h"
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout<<"____Creation du compte____"<< std::endl;
+    CompteBancaire user1(1, 1000);
+    std::cout << "____Consultation du compte____" << std::endl;
+    user1.consulteSolde();
+    std::cout << std::endl;
+
+    std::cout << "____Depot d argent de 500____" << std::endl;
+    user1.deposerArgent(500);
+    user1.consulteSolde();
+    std::cout << std::endl;
+
+    std::cout << "____Retrait d argent de 700____" << std::endl;
+    user1.retraitArgent(700);
+    user1.consulteSolde();
+    std::cout << std::endl;
+
+    std::cout << "____Retrait d argent de 1000____" << std::endl;
+    user1.retraitArgent(1000);
+    user1.consulteSolde();
+    std::cout << std::endl;
+
 }
